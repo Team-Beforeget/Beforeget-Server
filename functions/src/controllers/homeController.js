4d30c6bb26a16b13c351cc26bac65fb90fc33cec
young <tlszn121@sookmyung.ac.kr>
2022-01-09 17:48:17 +0900

    [ADD]: homeService

--- functions/src/controllers/homeController.js
+++ functions/src/controllers/homeController.js
@@ -1,26 +1,18 @@
 const util = require("../lib/util");
 const statusCode = require("../constants/statusCode");
 const responseMessage = require("../constants/responseMessage");
-const db = require('../database/db');
-const { postDB } = require('../database');
+const { getHomeService } = require('../services/homeServices');
 
-const media = ["Movie","Book","TV","Music","Webtoon","Youtube"]
 const getHomeController= async (req, res) => {
-    let client;
-    let data = {"Movie":0,"Book":0,"TV":0,"Music":0,"Webtoon":0,"Youtube":0}
-    try {
-      client = await db.connect(req);
-      const counts = await postDB.countPostsByMedia(client,req.user.id);
-      for(i of counts){
-          data[media[i['mediaId']-1]] = i['count'];
-      }
-      res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.REQUEST_SUCCESS, data));
-    } catch (error) {
-      console.log(error);
+
+    const data = await getHomeService(req);
+
+    if (data == -5){
       res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
-    } finally {
-      client.release();
+    }else{
+      res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.REQUEST_SUCCESS, data));
     }
+
   };
 
 module.exports = { getHomeController };
